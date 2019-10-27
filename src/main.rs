5e2a06c250e6d06245959c25d33adafc1b000c56
vmasdani <valianmasdani@gmail.com>
2019-10-28 00:32:28 +0700

    Major revision

--- src/main.rs
+++ src/main.rs
@@ -8,194 +8,214 @@
 
 extern crate chrono;
 extern crate qrcode;
-extern crate rusqlite;
+extern crate mosquitto_client as mosq;
+extern crate serde;
+extern crate serde_json;
 
-use actix_web::{web, App, HttpResponse, HttpServer};
-use actix_files::NamedFile;
-use actix_files as fs;
-use serde::Deserialize;
-use serde::Serialize;
+use mosq::Mosquitto;
+use serde::{Serialize, Deserialize};
 use std::thread;
 use std::process::Command;
 use std::error::Error;
 use std::time::Duration;
 use chrono::prelude::*;
-use rusqlite::{Connection, NO_PARAMS};
+use sqlite::State;
 use qrcode::QrCode;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
+struct ScheduleArray {
+    data: Vec<Schedule>
+}
+
+#[derive(Debug, Serialize, Deserialize)]
 struct Schedule {
-    id: u32,
-    hour: u32,
-    minute: u32,
+    id: i64,
+    hour: i64,
+    minute: i64,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct WateringTime {
-    id: u32,
-    minute: u32,
-    second: u32
+    minute: i64,
+    second: i64
 }
 
-// Control struct
-#[derive(Deserialize)]
-struct Control {
-    control_type: u8
-}
-
-fn index() -> actix_web::Result<NamedFile> {
-    Ok(NamedFile::open("./www/index.html")?)
-}
-
-fn control(control_json: web::Json<Control>) -> HttpResponse {
-    // TODO: Add GPIO control
-    HttpResponse::Ok()
-        .content_type("plain/text")
-        .body(format!("Control success! {}", control_json.control_type))
-}
-
-fn add_schedule(schedule_json: web::Json<Schedule>) -> HttpResponse {
-    let conn = Connection::open("./db/data.db3").unwrap();
+fn main() -> Result<(), Box <dyn Error>> {
+    // Starting database
+    thread::spawn(|| {
+        let connection = sqlite::open("./data.db").unwrap();
     
-    if let Ok(_res) = conn.execute("insert into schedule(hour, minute) values (?1, ?2)",
-        &[&schedule_json.hour, &schedule_json.minute]
-    ) { HttpResponse::Ok().body("Success adding schedule!") }
-    else { HttpResponse::Ok().body("Error adding schedule!") }
-}
-
-fn get_schedule() -> HttpResponse {
-    let conn = Connection::open("./db/data.db3").unwrap();
-
-    let mut query_get = conn.prepare("select * from schedule").unwrap();
-    let schedule_iter = query_get.query_map(NO_PARAMS, |row| Ok(Schedule {
-        id: row.get(0).unwrap(),
-        hour: row.get(1).unwrap(),
-        minute: row.get(2).unwrap()
-    })).unwrap();
-
-    let mut resp = String::from("[");
-
-    for schedule in schedule_iter {
-        let sched = schedule.unwrap();
-        resp = format!("{}{{\"id\":{}, \"hour\":{}, \"minute\":{}}},", resp, sched.id, sched.hour, sched.minute);
-        println!("Found sched: {:?}", sched);
-    }
-
-    let resp_len = resp.len();
-    
-    // Strip end comma
-    let resp_fmt = format!("{}]", &resp[0..resp_len - 1]);
-
-    HttpResponse::Ok()
-        .content_type("application/json")
-        // .body(format!("{}", query_get.column_count()))
-        .body(resp_fmt)
-}
-
-fn delete_schedule(path: web::Path<(u32,)>) -> HttpResponse {
-    let conn = Connection::open("./db/data.db3").unwrap();
-    
-    if let Ok(_res) = conn.execute("delete from schedule where id=?1",
-        &[&path.0]
-    ) { HttpResponse::Ok().body("Success deleting schedule!") }
-    else { HttpResponse::Ok().body("Error deleting schedule!") }
-}
-
-fn get_watering_time() -> HttpResponse {
-    let conn = Connection::open("./db/data.db3").unwrap();
-
-    let mut query_get = conn.prepare("select * from watering_time where id=1").unwrap();
-    let wt_iter = query_get.query_row(NO_PARAMS, |row| Ok(WateringTime {
-        id: row.get(0).unwrap(),
-        minute: row.get(1).unwrap(),
-        second: row.get(2).unwrap()
-    })).unwrap();
-
-    let found_wt = wt_iter;
-
-    HttpResponse::Ok().json(WateringTime {
-        id: found_wt.id,
-        minute: found_wt.minute,
-        second: found_wt.second
-    })
-}
-
-fn update_watering_time(wt_json: web::Json<WateringTime>) -> HttpResponse {
-    let conn = Connection::open("./db/data.db3").unwrap();
-    
-    if let Ok(_res) = conn.execute("update watering_time set minute=?1, second=?2 where id=1",
-        &[&wt_json.minute, &wt_json.second]
-    ) { HttpResponse::Ok().body("Success updating watering time!") }
-    else { HttpResponse::Ok().body("Error updating watering time!") }
-}
-
-fn poweroff() {
-    println!("Powering off!");
-    let res = Command::new("/sbin/poweroff").output().unwrap();
-    println!("{}", String::from_utf8_lossy(&res.stdout));
-}
-
-fn main() -> rusqlite::Result<()>{
-    // Create tables if not exists
-    let conn =  Connection::open("./db/data.db3").unwrap(); 
-
-    // Create schedule table
-    if let Ok(_res) = conn.execute(
+        // create schedule table
+        if let Err(e) = connection.execute(
         "
-            create table schedule (
-                id  integer primary key autoincrement,
-                hour integer,
-                minute integer
-            );
-            
-        ",
-        NO_PARAMS
-    ) {
-        println!("Create schedule table success!");
-    }
-    else {
-        println!("Create schedule table error! Already exists?");
-    }
-
-    // Create watering_time table
-    if let Ok(_res) = conn.execute(
-        "
-            create table watering_time (
+            create table schedule(
                 id integer primary key autoincrement,
-                minute integer,
-                second integer
-            )
-        ",
-        NO_PARAMS
-    ) {
-        println!("Create watering_time table success!");
-    }
-    else {
-        println!("Create watering_time table error! Already exists?");
-    }
+                hour tinyint,
+                minute tinyint
+            );                   
+        "
+        ) { println!("{}", e) }
 
-    // TODO: Add default watering time
-    let mut get_wt_statement = conn.prepare("select * from watering_time where id=1").unwrap();
-    let wt_iter = get_wt_statement.query_map(NO_PARAMS, |row| Ok(WateringTime {
-        id: row.get(0).unwrap(),
-        minute: row.get(1).unwrap(),
-        second: row.get(2).unwrap()
-    })).unwrap();
+        // create watering_time table
+        if let Err(e) = connection.execute(
+        "
+            create table watering_time(
+                id integer primary key autoincrement,
+                minute tinyint,
+                second tinyint
+            );                   
+        "
+        ) { println!("{}", e) }
 
-    let count = wt_iter.count();
-    if count == 0 {
-        println!("No watering time found! adding..");
-        if let Ok(_res) = conn.execute("insert into watering_time values(1, 0, 30)", NO_PARAMS) {
-            println!("Successfully added watering time table!");
+        connection.execute("insert into schedule(hour, minute) values(1, 5)");
+
+        // Check if watering_time exists
+        let mut wt_exist_stmt = connection.prepare("select * from watering_time where id=1").unwrap();
+
+        let mut wt_time_counter = 0;
+        while let State::Row = wt_exist_stmt.next().unwrap() {
+            wt_time_counter += 1;
+            println!("minute: {}, second: {}", wt_exist_stmt.read::<i64>(1).unwrap(), wt_exist_stmt.read::<i64>(2).unwrap());
+        }
+
+        println!("Num of watering_time = {}", wt_time_counter);
+        if wt_time_counter == 0 {
+            println!("Watering time is empty! Inserting....");
+            connection.execute("insert into watering_time(id, minute, second) values(1, 0, 10)");
         }
         else {
-            println!("Error adding watering time table!");
+            println!("Watering time is not empty!");
         }
-    }
-    else {
-        println!("Watering time found!");
-    }
+    });
+    
 
+    // MQTT Listener thread
+    thread::spawn(|| {
+        println!("MQTT thread started!");
+        let m = Mosquitto::new("client-1");
+        
+        m.connect("0.0.0.0", 1883);
+
+        let control = m.subscribe("control", 0).expect("Cannot subscribe to control topic!");
+        let schedule = m.subscribe("schedule/#", 0).expect("Cannot subscribe to schedule!");
+        let watering = m.subscribe("watering/#", 0).expect("Cannot subscribe to watering!");
+        let power = m.subscribe("power", 0).expect("Cannot subscribe to power!");
+        
+
+        let mut mc = m.callbacks(());
+        mc.on_message(|_,msg| {
+            let topic = msg.topic().to_string();
+            let message = msg.text().to_string();
+
+            match &topic[..] {
+                "schedule/req" => {
+                    println!("Schedule req topic detected!");
+                    
+                    let connection = sqlite::open("./data.db").unwrap();
+                    let mut schedule_stmt = connection.prepare("select * from schedule").unwrap();
+
+                    let mut schedule_array = ScheduleArray {
+                        data: Vec::new()
+                    };
+                        
+                    while let State::Row = schedule_stmt.next().unwrap() {
+                        let id = schedule_stmt.read::<i64>(0).unwrap();
+                        let hour = schedule_stmt.read::<i64>(1).unwrap();
+                        let minute = schedule_stmt.read::<i64>(2).unwrap();
+                        
+                        schedule_array.data.push(Schedule {
+                            id: id,
+                            hour: hour,
+                            minute: minute
+                        });
+                    }
+
+                    let schedule_json = serde_json::to_string(&schedule_array).unwrap();
+                    m.publish("schedule/res", &schedule_json[..].as_bytes(), 0, false).unwrap();
+                },
+                "schedule/add" => {
+                    println!("Schedule add topic detected!");
+                
+                    let data_json: serde_json::Value = serde_json::from_str(&message[..]).unwrap();
+                    let hour = &data_json["hour"];
+                    let minute = &data_json["minute"];
+                    
+                    println!("From message, hour: {}, minute: {}", hour, minute);
+
+                    let connection = sqlite::open("./data.db").unwrap();
+                    connection.execute(format!("insert into schedule(hour, minute) values({}, {})", hour, minute));
+                    println!("Schedule added successfully!");
+                },
+                "schedule/delete" => {
+                    println!("Schedule delete topic detected!");
+                
+                    let data_json: serde_json::Value = serde_json::from_str(&message[..]).unwrap();
+                    let id = &data_json["id"];
+                    
+                    println!("From message, id: {}", id);
+
+                    let connection = sqlite::open("./data.db").unwrap();
+                    connection.execute(format!("delete from schedule where id={}", id));
+                    println!("Schedule deleted successfully!");
+                },
+                
+                "watering/req" => {
+                    println!("Watering req topic detected!");
+
+                    let connection = sqlite::open("./data.db").unwrap();
+                    let mut wt_time_stmt = connection.prepare("select * from watering_time where id=1").unwrap();
+
+                    let mut watering_time = WateringTime {
+                        minute: 0,
+                        second: 0
+                    };
+
+                    while let State::Row = wt_time_stmt.next().unwrap() {
+                        let minute = wt_time_stmt.read::<i64>(1).unwrap();
+                        let second = wt_time_stmt.read::<i64>(2).unwrap();
+
+                        watering_time.minute = minute;
+                        watering_time.second = second;
+                    }
+
+                    let watering_time_json = serde_json::to_string(&watering_time).unwrap();
+                    m.publish("watering/res", &watering_time_json[..].as_bytes(), 0, false).unwrap();
+                },
+                "watering/update" => {
+                    
+                    let data_json: serde_json::Value = serde_json::from_str(&message[..]).unwrap();
+                    let minute = &data_json["minute"];
+                    let second = &data_json["second"];
+                    
+                    println!("From message, minute: {}, second: {}", minute, second);
+
+                    let connection = sqlite::open("./data.db").unwrap();
+                    connection.execute(format!("update watering_time set minute={}, second={} where id=1", minute, second));
+                    println!("Watering updated successfully!");
+                },
+                "control" => {
+                    // TODO: Add control GPIO functionalities
+                    println!("Control topic detected!");
+                    
+                    let data_json: serde_json::Value = serde_json::from_str(&message[..]).unwrap();
+                    let control_type = &data_json["control_type"];
+
+                    println!("Control data: {}", control_type);
+                },
+                "power" => {
+                    println!("Power topic detected!")
+                    // TODO: Make a system call to the command poweroff, shutting down machine
+                },
+                _ => println!("Topic irrelevant.")
+            }
+
+            println!("Topic: {}, Message: {}", topic, message);
+        });
+
+        m.loop_until_disconnect(200);
+    });
+
+    /*
     // Database poller thread
     thread::spawn(move || {
         let mut last_detected_time: DateTime<Local> = Local::now();
@@ -232,9 +252,11 @@ fn main() -> rusqlite::Result<()>{
             thread::sleep(Duration::from_secs(10));
         }
     });
+    */
 
     // IP checker thread
     thread::spawn(|| {
+        println!("IP poller thread started!");
         loop {
             if let Some(ip) = machine_ip::get() {
                 let detected_ip = ip.to_string();
@@ -254,24 +276,11 @@ fn main() -> rusqlite::Result<()>{
             thread::sleep(Duration::from_secs(60));
         }
     });
-
-    HttpServer::new(|| {
-        App::new()
-            .route("/control", web::post().to(control))
-            .route("/schedule", web::post().to(add_schedule))
-            .route("/schedule", web::get().to(get_schedule))
-            .route("/schedule/{schedule_id}", web::delete().to(delete_schedule))
-            .route("/watering-time", web::get().to(get_watering_time))
-            .route("/watering-time", web::put().to(update_watering_time))
-            .route("/poweroff", web::post().to(poweroff))
-            .service(fs::Files::new("/", "./www").index_file("index.html"))
-            //.route("/", web::get().to(index))
-            
-    })
-    .bind("0.0.0.0:8000")
-    .unwrap()
-    .run()
-    .unwrap();
+ 
+    // Keep main thread alive
+    loop {
+    
+    }
 
     Ok(())
 }
