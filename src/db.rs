b8e48859e38cd9f97edd98fd71a9e00aa6df495a
vmasdani <valianmasdani@gmail.com>
2020-05-22 17:31:29 +0000

    ignore client genrated folders

--- src/db.rs
+++ src/db.rs
@@ -1,7 +1,37 @@
-use rusqlite::{Connection, Result};
+use rusqlite::{params, Connection, Result};
 
 pub fn init() -> Result<Connection>{
     let conn = Connection::open("./data.db3")?;
-    
+ 
+    conn.execute("
+        create table if not exists schedule (
+            id integer primary key autoincrement,
+            hour integer,
+            minute integer
+        )
+    ", params![])?;
+
+    conn.execute("
+        create table if not exists watering_time (
+            id integer primary key autoincrement,
+            minute integer,
+            second integer
+        )
+    ", params![])?;
+
+    conn.execute("
+        create table if not exists sensor (
+            id integer primary key autoincrement,
+            serial_number  integer
+        )
+    ", params![])?;
+
+    conn.execute("
+        create table if not exists sensor_data (
+            id integer primary key autoincrement,
+            moisture integer
+        );
+    ", params![])?;
+            
     Ok(conn)
 }
